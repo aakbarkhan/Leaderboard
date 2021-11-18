302516d4e53d404a79bb388b24d53ac08fdfd63d
aakbarkhan <aakbarkhan@gmail.com>
2021-11-18 21:15:05 +0530

    Fix linters

--- src/index.js
+++ src/index.js
@@ -4,20 +4,18 @@ import sendScores from './api.js';
 const submit = document.querySelector('.submit');
 submit.addEventListener('click', sendScores);
 
-const getData = async() => {
-    const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/k7JsreU3WssGQsLJM2G4/scores/'
- const data = await fetch(url);
- const result =  await data.json();
- const listItems  = document.querySelector('.name-scores');
- listItems.innerHTML = '';
- result.result.forEach((item) => {
+const getData = async () => {
+  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FxX4jXjzFQDx4hE2gLhe/scores/';
+  const data = await fetch(url);
+  const result = await data.json();
+  const listItems = document.querySelector('.name-scores');
+  listItems.innerHTML = '';
+  result.result.forEach((item) => {
     listItems.innerHTML += `
     <p>${item.user}: ${item.score}</p>
-    `;    
-})
-document.querySelector('#name-input').value = '';
-document.querySelector('#score-input').value = '';
-}
+    `;
+  });
+};
 getData();
 const refreshBtn = document.querySelector('.btn');
 refreshBtn.addEventListener('click', getData);
